11fb1ccfab17152adb509ee52bfffad294d41f1d
veerrakshe <6111raksheveer@gmail.com>
2026-02-24 18:56:30 +0530

    Leetcode Problem 24 Feb

---
+++ Leetcode_DailyChallenge/src/Feb2026/SumOfRootToLeafBinaryNumbers_1022.java
@@ -0,0 +1,28 @@
+package Feb2026;
+
+import java.util.*;
+
+public class SumOfRootToLeafBinaryNumbers_1022 {
+	class Solution {
+
+		int solve(TreeNode root, int val) {
+
+			if (root == null) {
+				return 0;
+			}
+
+			val = (2 * val) + root.val;
+
+			// If leaf node
+			if (root.left == null && root.right == null) {
+				return val;
+			}
+
+			return solve(root.left, val) + solve(root.right, val);
+		}
+
+		public int sumRootToLeaf(TreeNode root) {
+			return solve(root, 0);
+		}
+	}
+}
