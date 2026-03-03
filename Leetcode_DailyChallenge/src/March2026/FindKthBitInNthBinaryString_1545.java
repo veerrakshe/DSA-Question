9a01ff06da51c455972fe094bd0c12ef45cc8307
veerrakshe <6111raksheveer@gmail.com>
2026-03-03 15:28:44 +0530

    Leetcode 3 march Problem

---
+++ Leetcode_DailyChallenge/src/March2026/FindKthBitInNthBinaryString_1545.java
@@ -0,0 +1,22 @@
+package March2026;
+
+public class FindKthBitInNthBinaryString_1545 {
+	class Solution {
+		public char findKthBit(int n, int k) {
+			if (n == 1) {
+				return '0';
+			}
+
+			int length = (1 << n) - 1;
+
+			if (k < Math.ceil(length / 2.0)) {
+				return findKthBit(n - 1, k);
+			} else if (k == Math.ceil(length / 2.0)) {
+				return '1';
+			} else {
+				char ch = findKthBit(n - 1, length - (k - 1));
+				return (ch == '0') ? '1' : '0';
+			}
+		}
+	}
+}
