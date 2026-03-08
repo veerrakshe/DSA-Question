aa752b7877de2b5f3d4460b0cca9e9f5baa9d713
veerrakshe <6111raksheveer@gmail.com>
2026-03-09 02:26:12 +0530

    Leetcode 8 March Problem

---
+++ Leetcode_DailyChallenge/src/March2026/FindUniqueBinaryString_1980.java
@@ -0,0 +1,16 @@
+package March2026;
+
+public class FindUniqueBinaryString_1980 {
+	class Solution {
+		public String findDifferentBinaryString(String[] nums) {
+			int n = nums.length;
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < n; i++) {
+				char flip = nums[i].charAt(i) == '0' ? '1' : '0';
+				sb.append(flip);
+			}
+			return sb.toString();
+
+		}
+	}
+}
