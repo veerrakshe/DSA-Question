538d1774e4b5491b588968e206d46fb2b55a4791
veerrakshe <6111raksheveer@gmail.com>
2026-03-13 23:22:31 +0530

    Leetcode 13 March Problem

---
+++ Leetcode_DailyChallenge/src/March2026/MinimumNumberOfSecondsToMakeMountainHeightZero_3296.java
@@ -0,0 +1,43 @@
+package March2026;
+
+public class MinimumNumberOfSecondsToMakeMountainHeightZero_3296 {
+	class Solution {
+		private boolean check(long mid, int[] workerTimes, int mH) {
+			long h = 0;
+
+			for (int t : workerTimes) {
+				h += (long) (Math.sqrt(2.0 * mid / t + 0.25) - 0.5);
+
+				if (h >= mH) {
+					return true;
+				}
+			}
+
+			return h >= mH;
+		}
+
+		public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+			int maxTime = 0;
+			for (int t : workerTimes)
+				maxTime = Math.max(maxTime, t);
+
+			long l = 1;
+			long r = (long) maxTime * mountainHeight * (mountainHeight + 1) / 2;
+
+			long result = 0;
+
+			while (l <= r) {
+				long mid = l + (r - l) / 2;
+
+				if (check(mid, workerTimes, mountainHeight)) {
+					result = mid;
+					r = mid - 1;
+				} else {
+					l = mid + 1;
+				}
+			}
+
+			return result;
+		}
+	}
+}
