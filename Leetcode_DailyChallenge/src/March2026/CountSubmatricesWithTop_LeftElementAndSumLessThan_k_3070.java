2f48b5d8d4a9be0f90258ad91bf81026683d42ac
veerrakshe <6111raksheveer@gmail.com>
2026-03-18 16:43:25 +0530

    Leetcode 18 March Problem

---
+++ Leetcode_DailyChallenge/src/March2026/CountSubmatricesWithTop_LeftElementAndSumLessThan_k_3070.java
@@ -0,0 +1,31 @@
+package March2026;
+
+public class CountSubmatricesWithTop_LeftElementAndSumLessThan_k_3070 {
+	class Solution {
+		public int countSubmatrices(int[][] grid, int k) {
+			int m = grid.length;
+			int n = grid[0].length;
+			int count = 0;
+
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+
+					if (i > 0)
+						grid[i][j] += grid[i - 1][j];
+					if (j > 0)
+						grid[i][j] += grid[i][j - 1];
+					if (i > 0 && j > 0)
+						grid[i][j] -= grid[i - 1][j - 1];
+
+					if (grid[i][j] <= k) {
+						count++;
+					} else {
+						break;
+					}
+				}
+			}
+
+			return count;
+		}
+	}
+}
