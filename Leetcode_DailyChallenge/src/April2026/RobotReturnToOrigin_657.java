f6905b48d18634fc43204552bf364d61c74b38a7
veerrakshe <6111raksheveer@gmail.com>
2026-04-05 13:00:38 +0530

    Leetcode 5th April Problem

---
+++ Leetcode_DailyChallenge/src/April2026/RobotReturnToOrigin_657.java
@@ -0,0 +1,23 @@
+package April2026;
+
+public class RobotReturnToOrigin_657 {
+	class Solution {
+		public boolean judgeCircle(String moves) {
+			int x = 0;
+			int y = 0;
+
+			for (char ch : moves.toCharArray()) {
+				if (ch == 'U')
+					y++;
+				else if (ch == 'D')
+					y--;
+				else if (ch == 'L')
+					x--;
+				else if (ch == 'R')
+					x++;
+			}
+
+			return x == 0 && y == 0;
+		}
+	}
+}
